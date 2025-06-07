a74876d7a3f12cd32118847916672a059bc7ff91
klao258 <klao25388@gmail.com>
2025-06-08 01:53:49 +0800

    最后优化日志

--- node/services/userMatchHandler.js
+++ node/services/userMatchHandler.js
@@ -6,6 +6,28 @@ const { dispatchPush } = require('./pushDispatcher');
 
 const whiteKeys = ['游戏', '余额', '流水', '返水', '反水']
 
+// 记录日志, 单用户最大记录 1000 条日志
+const insertUserLog = async (logData) => {
+  const { userId, username, nickname, groupId, groupName, sendDateTime, ...matchedGames } = logData;
+
+  // Step 1: 插入新日志（matchedAt 会自动写入）
+  await GameMatchLog.create(...logData);
+
+  // Step 2: 查询该用户日志总条数
+  const count = await GameMatchLog.countDocuments({ userId });
+
+  // Step 3: 如果超过 1000，删除最早的 10 条
+  if (count > 1000) {
+    const oldest = await GameMatchLog.find({ userId })
+      .sort({ matchedAt: 1 }) // 最早的日志
+      .limit(10)
+      .select('_id');
+
+    const ids = oldest.map(doc => doc._id);
+    await GameMatchLog.deleteMany({ _id: { $in: ids } });
+  }
+}
+
 /** 文本不超16，换行符不超1个，必须包含大于0的数字 */
 const isValidMessage = (text) => {
   const isLengthValid = text.length <= 16;
@@ -45,7 +67,7 @@ exports.handleMessage = async ({ groupId, groupName,  userId, username, nickname
   if (JSON.stringify(matchedGames) === '{}') return;
 
   // Step 1: 写入日志
-  await GameMatchLog.create({  userId,  username, nickname, groupId,  groupName, sendDateTime,  ...matchedGames });
+  await insertUserLog({ userId,  username, nickname, groupId,  groupName, sendDateTime,  ...matchedGames })
 
   // Step 2: 写入用户信息表
   let profile = await UserProfile.findOne({ userId });
