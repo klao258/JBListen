27ead15ddf0a0a9257eaf3ff6a08526cafb18e99
klao258 <klao25388@gmail.com>
2025-06-07 22:01:56 +0800

    增加消息推送

--- node/services/bot.js
+++ node/services/bot.js
@@ -5,9 +5,9 @@ const bot = new Telegraf(process.env.PUSHBOT_TOKEN);
 
 const TGPush = async(chatId, text) => {
     try {
-        // await bot.telegram.sendMessage(+chatId, text, {
-        //     parse_mode: 'HTML'  // ✅ 支持加粗、链接等
-        // });
+        await bot.telegram.sendMessage(+chatId, text, {
+            parse_mode: 'HTML'  // ✅ 支持加粗、链接等
+        });
     } catch (err) {
         console.error('❌ bot推送失败:', chatId, err.response?.description || err.message);
     }
