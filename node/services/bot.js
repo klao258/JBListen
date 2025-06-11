ba0890c2eb1c25a69423a7f3a83e395d81750712
klao258 <klao25388@gmail.com>
2025-06-11 20:37:43 +0800

    优化

--- node/services/bot.js
+++ node/services/bot.js
@@ -7,7 +7,7 @@ const bot2 = new Telegraf('7603105476:AAE1yQBSMgYdY3MBg-8GnTtTskLHRx3akSU');
 const TGPush = async(chatId, text) => {
     try {
         const random = Math.floor(Math.random() * 2) + 1;
-        const bot = random === 1 ? bot1 : bot2
+        const bot = +chatId === -4815235248 ? bot1 : random === 1 ? bot1 : bot2
         await bot.telegram.sendMessage(+chatId, text, {
             parse_mode: 'HTML'  // ✅ 支持加粗、链接等
         });
