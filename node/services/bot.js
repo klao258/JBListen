31c6c850d93bd2ef84971095e82551092e168c50
klao258 <klao25388@gmail.com>
2025-06-11 20:33:09 +0800

    两个机器人推送

--- node/services/bot.js
+++ node/services/bot.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const { Telegraf } = require('telegraf');
 
-const bot = new Telegraf(process.env.PUSHBOT_TOKEN);
+const bot1 = new Telegraf(process.env.PUSHBOT_TOKEN);
+const bot2 = new Telegraf('7603105476:AAE1yQBSMgYdY3MBg-8GnTtTskLHRx3akSU');
 
 const TGPush = async(chatId, text) => {
     try {
+        const random = Math.floor(Math.random() * 2) + 1;
+        const bot = random === 1 ? bot1 : bot2
         await bot.telegram.sendMessage(+chatId, text, {
             parse_mode: 'HTML'  // ✅ 支持加粗、链接等
         });
